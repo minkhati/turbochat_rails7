d76f51b742e8b7cf1fd279dbfbd3941092be0b06
minkhati <meetup.ror@gmail.com>
2022-05-30 22:45:53 +0000

    create user controller and participant model

--- app/models/room.rb
+++ app/models/room.rb
@@ -4,7 +4,25 @@ class Room < ApplicationRecord
   
   scope :public_rooms, -> { where(is_private: false) }
   
-  after_create_commit { broadcast_append_to "rooms" }
+  after_create_commit { broadcast_if_public }
   
   has_many :messages
+  has_many :participants, dependent: :destroy
+  
+  
+  def self.create_private_room(users, room_name)
+    single_room = Room.create(name: room_name, is_private: true)
+    
+    users.each do |user|
+      Participant.create(user_id: user.id, room_id: single_room.id)
+    end
+    single_room
+  end
+  
+  private
+  
+  def broadcast_if_public
+    broadcast_append_to "rooms" unless self.is_private
+  end
+  
 end
