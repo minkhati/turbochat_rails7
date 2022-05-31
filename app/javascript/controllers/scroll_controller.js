f668104e54b1543df73713c5392312e49b5a554a
minkhati <meetup.ror@gmail.com>
2022-05-31 17:25:35 +0000

    Add stimulus scroll position and clear form fields  after submit it

---
+++ app/javascript/controllers/scroll_controller.js
@@ -0,0 +1,22 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  /** on start **/
+  connect() {
+    console.log("Connected");
+    const messages = document.getElementById("messages");
+    messages.addEventListener("DOMNodeInserted", this.resetScroll)
+    this.resetScroll(messages)
+  }
+  
+  /** on Stop **/
+  disconnect() {
+    console.log("DisConnected");
+  }
+  
+  /** custom function **/
+  
+  resetScroll() {
+    messages.scrollTop = messages.scrollHeight - messages.clientHeight;
+  }
+}
